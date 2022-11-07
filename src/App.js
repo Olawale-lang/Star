a0c9479a635ef1adc7b6c6d98839146ea791da24
Olawale <bamideleolawale13@gmail.com>
2022-11-07 23:59:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Browser
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StateMatch from "./component/star.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main className='py-3'>
+        <Routes>
+          <Route path='/' element={<StateMatch />} exact></Route>
+        </Routes>
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
